30c3ea3bff1e96c1ff65a7794bcb52337eb37478
Santiagopt97 <santiagopineda99@gmail.com>
2025-03-26 17:42:18 -0500

    Agregar campos de contraseña y rol a la entidad Cliente

--- src/main/java/com/example/demo/models/Entity/Cliente.java
+++ src/main/java/com/example/demo/models/Entity/Cliente.java
@@ -21,9 +21,15 @@ public class Cliente implements Serializable {
     private int id;
     private String nombre, apellido, email;
 
+    @Column(name = "password")
+    private String password;
+
     @Column(name = "create_at")
     @Temporal(TemporalType.DATE)
     private Date createAt;
+
+    @Column(name = "role")
+    private String role;
 
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Encabezado> encabezados;
@@ -31,12 +37,14 @@ public class Cliente implements Serializable {
     public Cliente() {
     }
 
-    public Cliente(int id, String nombre, String apellido, String email, Date createAt) {
+    public Cliente(int id, String nombre, String apellido, String email, String password, Date createAt, String role) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
+        this.password = password;
         this.createAt = createAt;
+        this.role = role;
     }
 
     public int getId() {
@@ -71,12 +79,28 @@ public class Cliente implements Serializable {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public Date getCreateAt() {
         return createAt;
     }
 
     public void setCreateAt(Date createAt) {
         this.createAt = createAt;
+    }
+
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public List<Encabezado> getEncabezados() {
